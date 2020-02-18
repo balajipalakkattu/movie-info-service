e9dc849fdfc91b178fd0629d2fa59d165095b822
balajipalakkattu <balajipalakkattu@gmail.com>
2020-02-18 00:41:34 -0600

    Initial

---
+++ src/main/java/io/spring/start/movieinfoservive/MovieInfoServiveApplication.java
@@ -0,0 +1,13 @@
+package io.spring.start.movieinfoservive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieInfoServiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieInfoServiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/spring/start/movieinfoservive/MovieInfoServiveApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.start.movieinfoservive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
